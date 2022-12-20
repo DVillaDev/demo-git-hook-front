9ccc623512c535668bfca74625a0c371ec499d68
mac-DavidVilla <mac-davidvilla@MacBook-Pro-de-autentia.local>
2022-12-20 16:52:07 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react"
 
 function App() {
+
+  const [data, setData] = useState()
+
+  useEffect( () => {
+    text()
+  }, [])
+
+  const text = async() => {
+    const response = await fetch("http://localhost:8080/")
+    setData(await response.text())
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{data}</h2>
       </header>
     </div>
   );
