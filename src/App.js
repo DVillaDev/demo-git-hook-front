de3bdd8be919d9e723abd0bc165b763c2ed5c756
DVillaDev <david.villa@autentia.com>
2022-12-22 11:20:18 +0100

    demo

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>{data}</h2>
-        <h1>hola</h1>
+        <h1>demo</h1>
         <h2>{bye}</h2>
       </header>
     </div>
